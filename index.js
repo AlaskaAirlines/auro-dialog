abe479a491ffd10366e56f4662c3d034d1985ece
Jordan Jones <jmjones2463@gmail.com>
2024-10-03 12:52:38 -0700

    perf: refactor custom component registration config

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 import { AuroDialog } from './src/auro-dialog.js';
+import * as RuntimeUtils from '@aurodesignsystem/auro-library/scripts/utils/runtimeUtils.mjs';
 
-/**
- * Register Custom Element.
- * @param {Object} name - Name to use for custom element.
- * @returns {void}
- */
- const registerComponent = (name = 'custom-dialog') => {
-  // alias definition
-  if (!customElements.get(name)) {
-    customElements.define(name, class extends AuroDialog {});
-  }
-}
-
-export { registerComponent }
+RuntimeUtils.default.prototype.registerComponent('custom-dialog', AuroDialog);

b8fdfbb9f7abce189ab3458df35fec05e943c220
rmenner <rmenner@users.noreply.github.com>
2025-10-03 22:55:19 +0000

    deploy: 9f15cc35d87e813dfa87193c2f98503910ef328a

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-import { AuroDialog } from './src/auro-dialog.js';
+import { initBasicExample } from "../apiExamples/basic";
+import { initCustomExample } from "../apiExamples/custom";
+
+import { AuroDialog } from "../src/index";
 
 AuroDialog.register();
+AuroDialog.register("custom-dialog");
+
+export function initExamples(initCount) {
+  // biome-ignore lint/style/noParameterAssign: legacy error handling
+  initCount = initCount || 0;
+
+  try {
+    initBasicExample();
+    initCustomExample();
+  } catch (_err) {
+    if (initCount <= 20) {
+      // setTimeout handles issue where content is sometimes loaded after the functions get called
+      setTimeout(() => {
+        initExamples(initCount + 1);
+      }, 100);
+    }
+  }
+}
